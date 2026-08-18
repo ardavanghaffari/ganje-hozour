941891c1363a9f68fb208fec910eda1cdb226fcc
Ardavan Ghaffari <ghaffari.ardavan@gmail.com>
2026-08-18 14:54:27 +0200

    Restructure docs / add custom-toc to root page

--- asciidoctor.sh
+++ asciidoctor.sh
@@ -3,4 +3,7 @@
 webfonts="Vazirmatn:300,300italic,400,400italic,600,600italic,700,700italic"
 
 cd $(dirname $0)
-asciidoctor -a webfonts="$webfonts" -a docinfo=shared گنج_حضور.adoc
+find . -type f -name '*.adoc' -print0 |
+  xargs -0 asciidoctor \
+    -a webfonts="$webfonts" \
+    -a docinfo=shared
