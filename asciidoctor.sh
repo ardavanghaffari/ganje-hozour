86eaa00e420442456185712d7d614cc240a50be1
Ardavan Ghaffari <ghaffari.ardavan@gmail.com>
2026-08-21 20:42:01 +0200

    Generate HTML to build output

--- asciidoctor.sh
+++ asciidoctor.sh
@@ -3,7 +3,20 @@
 webfonts="Vazirmatn:300,300italic,400,400italic,600,600italic,700,700italic"
 
 cd $(dirname $0)
+
+ROOT=$(pwd)
+BUILD="$ROOT/build"
+MEDIA="$ROOT/media"
+
+rm -rf "$BUILD"
+mkdir -p "$BUILD"
+
+if [ -z "$CI" ]; then
+  ln -s "$MEDIA" "$BUILD/media"
+fi
+
 find . -type f -name '*.adoc' -print0 |
   xargs -0 asciidoctor \
+    -D "$BUILD" \
     -a webfonts="$webfonts" \
     -a docinfo=shared
