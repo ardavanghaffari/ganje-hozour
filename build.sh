5e3e1b5e152fa5944b284fd6c3a472d9a7049e06
Ardavan Ghaffari <ghaffari.ardavan@gmail.com>
2026-08-23 20:32:09 +0200

    Move adoc files to src/docs

--- build.sh
+++ build.sh
@@ -4,19 +4,13 @@ webfonts="Vazirmatn:300,300italic,400,400italic,600,600italic,700,700italic"
 
 cd $(dirname $0)
 
-ROOT=$(pwd)
-BUILD="$ROOT/build"
-MEDIA="$ROOT/media"
+build="$(pwd)/build"
 
-rm -rf "$BUILD"
-mkdir -p "$BUILD"
+rm -rf "$build"
+mkdir -p "$build"
 
-if [ -z "$CI" ]; then
-  ln -s "$MEDIA" "$BUILD/media"
-fi
-
-find . -type f -name '*.adoc' -print0 |
+find src/docs -type f -name '*.adoc' -print0 |
   xargs -0 asciidoctor \
-    -D "$BUILD" \
+    -D "$build" \
     -a webfonts="$webfonts" \
     -a docinfo=shared
