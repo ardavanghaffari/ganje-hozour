e1fb363286ebe4b36a3b8d2c15aa3894890fb522
Ardavan Ghaffari <ghaffari.ardavan@gmail.com>
2026-09-05 16:59:27 +0200

    Split audio excerpts according to program number

--- build.sh
+++ build.sh
@@ -5,12 +5,25 @@ webfonts="Vazirmatn:300,300italic,400,400italic,600,600italic,700,700italic"
 cd $(dirname $0)
 
 build="$(pwd)/build"
+docs="$(pwd)/src/docs"
 
 rm -rf "$build"
 mkdir -p "$build"
 
-find src/docs -type f -name '*.adoc' -print0 |
-  xargs -0 asciidoctor \
-    -D "$build" \
-    -a webfonts="$webfonts" \
-    -a docinfo=shared
+find "$docs" -type f -name '*.adoc' -print0 |
+  while IFS= read -r -d '' file; do
+
+    echo "Processing $file"
+
+    relative_path="${file#"$docs"/}"
+    output_dir="$build/$(dirname "$relative_path")"
+
+    mkdir -p "$output_dir"
+
+    asciidoctor \
+      -a webfonts="$webfonts" \
+      -a docinfodir="$docs" \
+      -a docinfo=shared \
+      -D "$output_dir" \
+      "$file"
+  done
